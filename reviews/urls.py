42defc50b9b4191b6bd24d32c92924bd318a8dd8
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-07 07:19:27 +0000

    basic form submission

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from . import views
+
+
 urlpatterns = [
-    path("", views.reviews_index,  name = "reviews-index")
+    path("", views.reviews_index,  name = "reviews-index"),
+    path("submitted/", views.review_submitted, name="review-submitted")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def reviews_index(request):
+    if request.method == "POST":
+        print(request.POST)
+        return HttpResponseRedirect("/submitted")
     return render(request, "reviews/index.html",)
+
+
+def review_submitted(request):
+    return render(request, "reviews/submitted.html",)
