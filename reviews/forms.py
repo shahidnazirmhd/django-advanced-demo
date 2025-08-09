ef1625cb7ba2d2de41d4dfff895a2b974ade61db
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-09 08:16:15 +0000

    form using ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,22 +1,52 @@
 from django import forms
 
 
-class ReviewForm(forms.Form):
-    full_name = forms.CharField(max_length=100, min_length=2, error_messages={
-        "required": "Your name must not be empty!",
-        "max_length": "Please enter a shorter name!",
-        "min_length": "Please enter a valid name",
-    })
-    email = forms.EmailField(error_messages={
-        "required": "Email is required",
-    })
-    rating = forms.IntegerField(min_value=1, max_value=5, error_messages={
-        "required": "Rating is required",
-        "min_value": "Please rate correctly (1 t0 5)!",
-        "max_value": "Please rate correctly (1 t0 5)!",
-    })
-    message = forms.CharField(widget=forms.Textarea, max_length=500, min_length=3, error_messages={
-        "required": "Feedback message must not be empty!",
-        "max_length": "Please give a short feedback message!",
-        "min_length": "Please give valid feedback message",
-    })
+from .models import Review
+
+
+# class ReviewForm(forms.Form):
+#     full_name = forms.CharField(max_length=100, min_length=2, error_messages={
+#         "required": "Your name must not be empty!",
+#         "max_length": "Please enter a shorter name!",
+#         "min_length": "Please enter a valid name",
+#     })
+#     email = forms.EmailField(error_messages={
+#         "required": "Email is required",
+#     })
+#     rating = forms.IntegerField(min_value=1, max_value=5, error_messages={
+#         "required": "Rating is required",
+#         "min_value": "Please rate correctly (1 t0 5)!",
+#         "max_value": "Please rate correctly (1 t0 5)!",
+#     })
+#     message = forms.CharField(widget=forms.Textarea, max_length=500, min_length=3, error_messages={
+#         "required": "Feedback message must not be empty!",
+#         "max_length": "Please give a short feedback message!",
+#         "min_length": "Please give valid feedback message",
+#     })
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        #fields = ["message"]
+        fields = "__all__"
+        # exclude = ['owner_comment']
+        labels = {
+            "full_name": "Your Name",
+            "message": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "full_name": {
+              "required": "Your name must not be empty!",
+              "max_length": "Please enter a shorter name!"
+            },
+            "email": {
+                "required": "Email is required",
+            },
+            "rating": {
+                "required": "Rating is required",
+            },
+            "message": {
+                "required": "Feedback message must not be empty!",
+                "max_length": "Please give a short feedback message!",
+            }
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -6,15 +6,16 @@ from .models import Review
 # Create your views here.
 def reviews_index(request):
     if request.method == "POST":
-        review_form = forms.ReviewForm(request.POST)
+        review_form = forms.ReviewForm(request.POST)   # Use instance parameter for update in DB
         if review_form.is_valid():
             print(review_form.cleaned_data)
-            Review.objects.create(
-                full_name=review_form.cleaned_data["full_name"],
-                email=review_form.cleaned_data["email"],
-                rating=review_form.cleaned_data["rating"],
-                message=review_form.cleaned_data["message"]
-            )
+            # Review.objects.create(
+            #     full_name=review_form.cleaned_data["full_name"],
+            #     email=review_form.cleaned_data["email"],
+            #     rating=review_form.cleaned_data["rating"],
+            #     message=review_form.cleaned_data["message"]
+            # )
+            review_form.save()
             return HttpResponseRedirect("/submitted")
     else:
         review_form = forms.ReviewForm()
