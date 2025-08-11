402c813a32615ecab2c9a4314c0981527718eb0e
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-11 05:54:25 +0000

    Using TemplateView

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view(),  name = "reviews-index"),
-    path("submitted/", views.review_submitted, name="review-submitted")
+    path("submitted/", views.SubittedView.as_view(), name="review-submitted"),
+    path("reviews/", views.AllReviewsView.as_view(), name="all-reviews"),
+    path("reviews/<int:id>", views.ReviewDetailView.as_view(), name="review-detail")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+
+from .models import Review
 
 from . import forms
 
@@ -23,5 +26,36 @@ class ReviewView(View):
 
 
 
-def review_submitted(request):
-    return render(request, "reviews/submitted.html",)
+# def review_submitted(request):
+#     return render(request, "reviews/submitted.html",)
+class SubittedView(TemplateView):
+    template_name = "reviews/submitted.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["emoji"] = "🫶"
+        return context
+    
+
+class AllReviewsView(TemplateView):
+    template_name = "reviews/all-reviews.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews 
+        return context
+    
+
+class ReviewDetailView(TemplateView):
+    template_name = "reviews/review-detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        review = Review.objects.get(pk=review_id)
+        context["review"] = review
+        return context
+    
+    
+    
