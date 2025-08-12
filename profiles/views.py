1d3113eaadf5bd22b988884f76b4643cdc3ab047
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-12 09:45:34 +0000

    Using CreateView for File upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, FormView
 
 from .forms import UserProfileForm
 from .models import UserProfile
 
 # Create your views here.
-class CreateProfileView(View):
-    def get(self, request):
-        form = UserProfileForm()
-        return render(request, "profiles/index.html", { "form": form})
-
-    def post(self, request):
-        submitted_form = UserProfileForm(request.POST, request.FILES)
-        print("TESTETSTEST")
-        if submitted_form.is_valid():
-            # profile = UserProfile(profile=request.FILES["profile"])
-            # profile.save()
-            submitted_form.save()
-            return HttpResponseRedirect("/profiles/")
-        return HttpResponseRedirect("/profiles/", {"form": submitted_form})
+class CreateProfileView(CreateView):
+    template_name = "profiles/index.html"
+    model = UserProfile
+    form_class = UserProfileForm
+    
+    success_url = "/profiles"
